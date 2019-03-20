d33f6a6ab6060aa5b2ddad17b6bc6c47c688f72d
Abdul <awsayed@alaska.edu>
2019-03-20 06:20:09 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,35 @@
 //define functions here
 
+function getIt() {
+    $("p").on("click", function () {
+        alert("Hey!");
+    });
+}
+
+function frameIt() {
+    $('img').on("load", function() {
+        $(img).addClass("tasty");
+    })
+}
+
+
+function pressIt() {
+    $(document).on("keydown", function(e) {
+        console.log(e.key)
+        if (e.key == 'G') {
+            alert('You pressed G!!!');
+        }
+    })
+}
+
+
+function submitIt() {
+    $('form').on("submit", function() {
+        alert("Your form is going to be submitted now.");
+    })
+}
+
+
 $(document).ready(function(){
 
 // call functions here
